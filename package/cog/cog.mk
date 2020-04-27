1dd26eed7cbedd35d391f74a84132bc39deff93f
Miguel Gomez <magomez@igalia.com>
2020-04-27 15:53:21 +0200

    [wpe][cog] Upgraded WPE to 2.28.2 and cog to 0.6

--- package/cog/cog.mk
+++ package/cog/cog.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-#COG_VERSION = 0.4.0
-#COG_SITE = https://wpewebkit.org/releases
-#COG_SOURCE = cog-$(COG_VERSION).tar.xz
-COG_VERSION = e7ff11cb31cbc8da315283670c1f2b4922e09331
-COG_SITE = $(call github,igalia,cog,$(COG_VERSION))
+COG_VERSION = 0.6.0
+COG_SITE = https://wpewebkit.org/releases
+COG_SOURCE = cog-$(COG_VERSION).tar.xz
 COG_INSTALL_STAGING = YES
 COG_DEPENDENCIES = dbus wpewebkit libinput
 COG_LICENSE = MIT
